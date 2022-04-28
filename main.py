331fd54063fdcae62f87d538eb299962da78fd5e
pingan.kong <pingan.kong@asiainnovations.com>
2022-04-28 15:23:53 +0800

    访问http测试网站，并创建了UA本地代理池

--- main.py
+++ main.py
@@ -16,56 +16,7 @@ def getBaiduHtmlContent():
     html = response.read().decode('utf-8')
     print(html)
 
-
-def getHttpbin():
-    # 向网站发送get请求
-    response = request.urlopen('http://httpbin.org/get')
-    html = response.read().decode()
-    print(html)
-
-
-#     {
-#   "args": {},
-#   "headers": {
-#     "Accept-Encoding": "identity",
-#     "Host": "httpbin.org",
-#     "User-Agent": "Python-urllib/3.8",  很明显这里做了反爬虫处理，UA，所以我们需要重构爬虫UA信息
-#     "X-Amzn-Trace-Id": "Root=1-626a3e68-1257c1e25598a0a63a32f341"
-#   },
-#   "origin": "137.59.102.104",
-#   "url": "http://httpbin.org/get"
-# }
-
-# ============================对比
-
-# {
-#   "args": {},
-#   "headers": {
-#     "Accept-Encoding": "identity",
-#     "Host": "httpbin.org",
-#     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:65.0) Gecko/20100101 Firefox/65.0", //模拟成了MAC电脑访问
-#     "X-Amzn-Trace-Id": "Root=1-626a3f2b-654d31a139b1c68c49018f98"
-#   },
-#   "origin": "137.59.102.104",
-#   "url": "http://httpbin.org/get"
-# }
-
-def refactor_get_httpbin():
-    # 定义变量：URL 与 headers
-    url = 'http://httpbin.org/get'  # 向测试网站发送请求
-    # 重构请求头，伪装成 Mac火狐浏览器访问，可以使用上表中任意浏览器的UA信息
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:65.0) Gecko/20100101 Firefox/65.0'}
-    # 1、创建请求对象，包装ua信息
-    req = request.Request(url=url, headers=headers)
-    # 2、发送请求，获取响应对象
-    res = request.urlopen(req)
-    # 3、提取响应内容
-    html = res.read().decode('utf-8')
-    print(html)
-
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # getHttpbin()
-    refactor_get_httpbin()
+    getBaiduHtmlContent()
---
+++ test-fake-useragent.py
@@ -0,0 +1,19 @@
+from fake_useragent import UserAgent
+
+# 实例化一个对象
+ua = UserAgent()
+# 随机获取一个ie浏览器ua
+print(ua.ie)
+print(ua.ie)
+# 随机获取一个火狐浏览器ua
+print(ua.firefox)
+print(ua.firefox)
+
+# 随机获取IE的UA
+# Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/4.0; GTB7.4; InfoPath.3; SV1; .NET CLR 3.1.76908; WOW64; en-US)
+# Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.2; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)
+
+# 随机获取火狐的UA
+# Mozilla/5.0 (Windows NT 6.2; Win64; x64; rv:16.0.1) Gecko/20121011 Firefox/21.0.1
+# Mozilla/5.0 (Windows NT 6.1; WOW64; rv:21.0) Gecko/20130401 Firefox/21.0
+
